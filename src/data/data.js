b37ab145a4848f18e76a18128229673176f00fde
Mehul Singh Teya <mehulsaini456@gmail.com>
2023-09-17 00:45:20 +0530

    feature(src/assets, src/data): Add normal assets, icons and dummy data

---
+++ src/data/data.js
@@ -0,0 +1,88 @@
+import STATICS from "../enums";
+
+// import modernVase from "../assets/modernVase.png";
+// import modernDecor from "../assets/modernDecor.png";
+// import stylishProps from "../assets/stylishProps.png";
+// import modernUtensils from "../assets/modernUtensils.png";
+// import fragranceFlowers from "../assets/fragranceFlowers.png";
+
+export const navbarLinksData = [
+  {
+    id: STATICS.HOME,
+    label: STATICS.HOME,
+    link: "#",
+  },
+  {
+    id: STATICS.ELECTRONICS,
+    label: STATICS.ELECTRONICS,
+    link: "#",
+  },
+  {
+    id: STATICS.BOOKS,
+    label: STATICS.BOOKS,
+    link: "#",
+  },
+  {
+    id: STATICS.MUSIC,
+    label: STATICS.MUSIC,
+    link: "#",
+  },
+  {
+    id: STATICS.MOVIES,
+    label: STATICS.MOVIES,
+    link: "#",
+  },
+  {
+    id: STATICS.GAMES,
+    label: STATICS.GAMES,
+    link: "#",
+  },
+  {
+    id: STATICS.FURNITURE,
+    label: STATICS.FURNITURE,
+    link: "#",
+  },
+  {
+    id: STATICS.TRAVEL,
+    label: STATICS.TRAVEL,
+    link: "#",
+  },
+  {
+    id: STATICS.BOTANICAL,
+    label: STATICS.BOTANICAL,
+    link: "#",
+  },
+];
+
+export const carousalData = [
+  {
+    id: STATICS.MODERN_KITCHEN_UTENSILS,
+    label: `0 - ${STATICS.MODERN_KITCHEN_UTENSILS}`,
+    // imgSrc: modernUtensils,
+    imgSrc: "https://picsum.photos/id/10/800/600",
+  },
+  {
+    id: STATICS.STYLISH_PROPS,
+    label: `1 - ${STATICS.STYLISH_PROPS}`,
+    // imgSrc: stylishProps,
+    imgSrc: "https://picsum.photos/id/11/800/600",
+  },
+  {
+    id: STATICS.MODERN_DECOR,
+    label: `2 - ${STATICS.MODERN_DECOR}`,
+    // imgSrc: modernDecor,
+    imgSrc: "https://picsum.photos/id/13/800/600",
+  },
+  {
+    id: STATICS.MODERN_VASE,
+    label: `3 - ${STATICS.MODERN_VASE}`,
+    // imgSrc: modernVase,
+    imgSrc: "https://picsum.photos/id/14/800/600",
+  },
+  {
+    id: STATICS.FRANGRANCE_FLOWERS,
+    label: `4 - ${STATICS.FRANGRANCE_FLOWERS}`,
+    // imgSrc: fragranceFlowers,
+    imgSrc: "https://picsum.photos/id/15/800/600",
+  },
+];
